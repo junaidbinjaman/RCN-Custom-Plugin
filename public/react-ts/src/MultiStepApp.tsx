e2dae158071a46784b955c966596b2c545e0b29b
Junaid Bin Jaman <junaidbinjaman@Junaids-MacBook-Pro.local>
2023-09-21 13:05:51 +0600

    Added react ts into the plugin folder

---
+++ public/react-ts/src/MultiStepApp.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MultiStepApp = () => {
+    return (
+        <div>
+            <h1>hello, World from tsx</h1>
+        </div>
+    );
+};
+
+export default MultiStepApp;
