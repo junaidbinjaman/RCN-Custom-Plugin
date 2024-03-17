8e1923f8bf7e6d61d22d41b570f58e3ed3fd0798
junaidbinjaman <me@junaidbinjaman.com>
2024-03-17 19:01:37 +0600

    Development starts here

---
+++ includes/class-stbw-utility.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The file that defines the core plugin class
+ *
+ * @link       https://junaidbinjaman.com
+ * @since      1.2.0
+ *
+ * @package    Stbw
+ * @subpackage Stbw/includes
+ */
+
+/**
+ * The utility function container class.
+ *
+ * This is used to define internationalization, admin-specific hooks, and
+ * public-facing site hooks.
+ *
+ * Also maintains the unique identifier of this plugin as well as the current
+ * version of the plugin.
+ *
+ * @since      1.0.0
+ * @package    Stbw
+ * @subpackage Stbw/includes
+ * @author     Junaid Bin Jaman <me@junaidbinjaman.com>
+ */
+class Stbw_Utility {
+
+	/**
+	 * Undocumented function
+	 *
+	 * @return string
+	 */
+	public function foo() {
+		return 'Hello';
+	}
+}
