72170ef09752da94ef6f5577d0cd15fce3c1182f
junaidbinjaman <me@junaidbinjaman.com>
2024-03-29 02:53:34 +0600

    The frist utility function is written

--- public/js/stbw-public.js
+++ public/js/stbw-public.js
@@ -1,32 +1,39 @@
-(function( $ ) {
-	'use strict';
+(function ($) {
+  'use strict';
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+  /**
+   * All of the code for your public-facing JavaScript source
+   * should reside in this file.
+   *
+   * Note: It has been assumed you will write jQuery code here, so the
+   * $ function reference has been prepared for usage within the scope
+   * of this function.
+   *
+   * This enables you to define handlers, for when the DOM is ready:
+   */
 
-})( jQuery );
+  $(function () {
+    const data = [
+      {name: 'Junaid Bin jaman', email: 'me@junaidbinjaman.com'},
+      {name: 'Junaid Bin jaman 1', email: 'me@junaidbinjaman.com'},
+      {name: 'Junaid Bin jaman 2', email: 'me@junaidbinjaman.com'},
+    ];
+    foobar(data);
+    let mobileNumber1 = '+800 1705294083';
+	mobileNumber1 = mobileNumber1.replace(/[ ]/g, '');
+
+	console.log(
+      validator.isNumeric(mobileNumber1)
+    );
+  });
+})(jQuery);
+
+function foobar(data) {
+  for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+
+    for (const key in element) {
+      console.log(element[key]);
+    }
+  }
+}
